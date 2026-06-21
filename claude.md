7c588f30ed6cd79eb863a0647d115b8dcb296b58
Tanner Hamilton <tannerhamilton2000@gmail.com>
2026-06-21 16:16:41 -0700

    Set up messaging with Claude model Basing index html to send a message to API API sends question to claude and returns response Response is updated onto chat in frontend.

---
+++ claude.md
@@ -0,0 +1,5 @@
+This is a retrieval augmented generation project using claude.
+
+A user will upload documents to the website in specific bubbles, then ask question to claude about their project that the files relate to. This is focused towards my experience in civil engineering. With design guideline documents or Requests For Information that need to be answered. 
+
+Please walk me through the process of getting this set up. This is going to be a portfolio project, but I also want to learn all of this as I go, so please do not generate the code for the hard stuff. 
--- main.py
+++ main.py
@@ -1,7 +1,23 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile
+from pydantic import BaseModel
+import anthropic
+from dotenv import load_dotenv
+from pypdf import PdfReader
 
+load_dotenv()
+
+from fastapi.middleware.cors import CORSMiddleware
+
+
+#Stores all the info from uploaded documents
+document_store = {}
 app = FastAPI()
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 async def root():
@@ -10,3 +26,39 @@ async def root():
 @app.get("/number/{number}")
 async def get_number(number: int):
     return {"message": f"The number is {number}"}
+
+class Request(BaseModel):
+    question: str
+
+class UploadedDocument(BaseModel):
+    name: str
+    filepath: str
+
+client = anthropic.Anthropic()
+@app.post("/ask_question")
+def ask_question(request: Request):
+    message = client.messages.create(
+        model="claude-haiku-4-5",
+        max_tokens=1024,
+        system = "You're a civil engineering assistant. " \
+        "Do not assume calculations or answers. " \
+        "Only reference known knowledge, and reference any facts you make.",
+        
+        messages=[{
+            "role":"user",
+            "content":request.question
+            }]
+        )
+    return {"answer":message.content[0].text}
+
+@app.post("/upload_file")
+def upload_file(file: UploadFile, category:str):
+    
+    doc = PdfReader(file)
+    text = ""
+    for page in doc.pages:
+        text+= page.extract_text()
+    document_store[file.name] = text
+
+
+    
