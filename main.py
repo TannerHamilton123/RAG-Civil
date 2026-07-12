a3fe76501765ae366364788f80e676e0a5a522a8
Tanner Hamilton <tannerhamilton2000@gmail.com>
2026-07-12 14:55:43 -0700

    Finished up the chunking and indexing
    I successfully chunked the text of each document
    and gave each chunk an embedding or vector.
    
    Then I indexed the question and compared it
    against the chunks using cosine similarity and returned
    the 5 most similar chunks.

--- main.py
+++ main.py
@@ -4,6 +4,14 @@ import anthropic
 import io
 from dotenv import load_dotenv
 from pypdf import PdfReader
+import nltk
+from openai import OpenAI
+import heapq
+import numpy as np #Numpy and norm are for cosine similarity between vectors
+from numpy.linalg import norm
+
+nltk.download('punkt_tab')
+from nltk.tokenize import sent_tokenize
 
 load_dotenv()
 
@@ -20,31 +28,56 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+
+def get_embedding(text):
+    response = OpenAI().embeddings.create(
+        input=text,
+        model="text-embedding-3-small"
+    )
+    return response.data[0].embedding
+
+def get_relevant_chunks(question):
+    question_embedding = get_embedding(question)
+    
+    chunk_similarity = {}
+    for file_name, chunk_dict in document_store.items():
+        for chunk_number in chunk_dict:
+            current_chunk = chunk_dict[chunk_number]
+            A = current_chunk["embedding"]
+            B = question_embedding
+
+            # compute cosine similarity
+            cosine_similarity= np.dot(A, B) / (norm(A) * norm(B))
+
+            chunk_info = f"{file_name} page {current_chunk['page_number']}: {current_chunk['text']}"
+            chunk_similarity[chunk_info] = cosine_similarity
+
+    top_items = heapq.nlargest(5, chunk_similarity.items(), key=lambda item: item[1])
+    relevant_chunks_text = ""
+    for chunk_info, similarity in top_items:
+        relevant_chunks_text += str(chunk_info) + "\n"
+        
+
+
+    return relevant_chunks_text
+
+
+
+
+
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
-@app.get("/number/{number}")
-async def get_number(number: int):
-    return {"message": f"The number is {number}"}
 
 class Request(BaseModel):
     question: str
-
-# class UploadedDocument(BaseModel):
-#     name: str
-#     filepath: str
 
 client = anthropic.Anthropic()
 @app.post("/ask_question")
 def ask_question(request: Request):
 
-    document_context = ""
-    for file in document_store:
-        text = f"Document{file}: {document_store[file]}\n"
-        document_context += text
-
-
+    relevant_chunks = get_relevant_chunks(request.question)
     message = client.messages.create(
         model="claude-haiku-4-5",
         max_tokens=1024,
@@ -55,24 +88,66 @@ def ask_question(request: Request):
         
         messages=[{
             "role":"user",
-            "content": f"Here are the project documents:\n\n{document_context}\n\nQuestion: {request.question}"
+            "content": f"Here are the project documents:\n\n{relevant_chunks}\n\nQuestion: {request.question}"
             }]
         )
     return {"answer":message.content[0].text}
 
 
 
+
+
+#The function scans the uploaded file.
+#If the file is pdf, the text is scanned for each page and divided into sentences.
+#Each sentence is added to a chunk of maximum token size.
+#The document store is stored as a dictionary with the file name as the key and a list of chunks as the value.
+#each chunk is a dictionary with a page number, text, and vector
+
 @app.post("/upload_file")
 async def upload_file(file: UploadFile = File(...), name:str = Form(...)):
+    token_chunk = 100
     contents = await file.read()
+    chunk_number = 1
 
     if name.endswith(".pdf"):
         #convert bytes to readable format
         reader = PdfReader(io.BytesIO(contents))
         text = ""
+        page_number = 0
+        document_store[name] = {}
         for page in reader.pages:
-            text += page.extract_text()
-        document_store[name] = text
+    
+            page_number += 1
+            token_count = 0
+            
+            
+            sentences = sent_tokenize(page.extract_text())
+
+            #For each sentence in the page, check if sentence would make the chunk token count larger than max. 
+            for sentence in sentences:
+                approx_tokens = len(sentence.split()) * 1.3 #an estimate of tokens per word
+                if token_count + approx_tokens <= token_chunk:
+                    text += sentence
+                    token_count += approx_tokens
+                else:
+                    #if not, then store the current text, and create a new chunk.
+                    document_store[name][chunk_number] = {}
+                    document_store[name][chunk_number]["text"] = text
+                    document_store[name][chunk_number]["embedding"] = get_embedding(text)
+                    document_store[name][chunk_number]["page_number"] = page_number
+                    token_count = approx_tokens
+                    text = sentence
+                    chunk_number += 1
+                
+            document_store[name][chunk_number] = {}
+            document_store[name][chunk_number]["text"] = text
+            document_store[name][chunk_number]["embedding"] = get_embedding(text)
+            document_store[name][chunk_number]["page_number"] = page_number
+            #reset for next page
+            chunk_number += 1
+            text = ""
+
+
         print("document" + name + " uploaded")
 
     return {"message": f"Stored document under '{name}'"}
